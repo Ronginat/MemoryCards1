5804cf2f22cf6cc52ff147241c9524afda4de12b
ronginat <ronginat51@gmail>
2018-03-31 21:58:09 +0300

    added gui to main activity

---
+++ AndroidStudioProjects/MemoryCards1/app/src/main/java/afeka/com/memorycards1/Strings.java
@@ -0,0 +1,6 @@
+package afeka.com.memorycards1;
+
+
+class Strings {
+    static final String wrong_input_main_activity = "You have to fill your name and age";
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Parallel-K_MEANS
